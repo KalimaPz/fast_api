b057d537487c3be798764ff37c6106f13fc1b570
Kalimapz <kalima007c@gmail.com>
2021-08-27 15:17:11 +0700

    fix logic

--- main.py
+++ main.py
@@ -5,17 +5,18 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     name: str
-
+    faculty : str
+    dept : str
+    
 app = FastAPI()
 users = [
     {
         "id":1,
-        "name" : "Donnukrit"
+        "name" : "Donnukrit",
+        "faculty" : "Engineering",
+        "dept" : "Computer Engineering"
     },
-     {
-        "id":2,
-        "name" : "John Doe"
-    }
+     
 ]
 @app.get("/")
 def root_page() :
@@ -32,13 +33,17 @@ async def createUser(user:User):
         if len(users) == 0 :
             users.append({
                 "id" : 0,
-                "name" : user.name 
+                "name" : user.name ,
+                "faculty" : user.faculty,
+                "dept": user.dept
             })
     
         else:
             users.append({
                 "id" : last_idx + 1,
-                "name" : user.name
+                "name" : user.name ,
+                "faculty" : user.faculty,
+                "dept": user.dept
             })
         return {
             "msg" : True,
@@ -53,16 +58,41 @@ async def createUser(user:User):
     
 @app.get("/getUserById/{user_id}")
 async def getUserById(user_id:int) :
-    for user in users:
-        if user["id"] == user_id:
-            return {
-                "msg" : True,
-                "data" : user
-            }
-        else :
-            return {
-                "msg" : False,
-                "data" : []
-            }
+    try:
+        for user in users:
+            if user_id is user["id"]:
+                return {
+                    "msg" : True,
+                    "data" : user
+                }
+        return {
+            "msg" : False,
+            "data" : "User does not exist."
+        }
+            
+    except: 
+        return {
+            "msg" : False,
+            "data" : "exception occured."
+    }
 
+@app.delete("/deleteUser/{user_id}")
+async def deleteUserById(user_id:int):
+    for user in users:
+        if user_id == user["id"]:
+            print(user)
+            remove_index = users.index(user)
+            users.pop(remove_index)
+            return {
+                "msg":True,
+                "data" : "User has been delete"
+            }
+        else: 
+            return {
+                "msg":False,
+                "data" : "User does not exist."
+            }
+            
+            
     
+            
